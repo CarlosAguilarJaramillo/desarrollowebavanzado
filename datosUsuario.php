d1a2362a8ce6feb62499a99cdc772fd4d8010684
CarlosAguilarJaramillo <54157664+CarlosAguilarJaramillo@users.noreply.github.com>
2020-01-05 22:38:09 -0600

    Primer commit de desarrollo web avanzado

---
+++ datosUsuario.php
@@ -0,0 +1,83 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="http://fonts.googleapis.com/css?family=Bitter" rel="stylesheet" type="text/css">
+	<link href="estilo.css" rel="stylesheet" type="text/css">
+
+	<title>Datos de usuario</title>
+</head>
+
+<body>
+
+	<?php 
+
+//Abre el archivo y si no existe lo crea
+fopen("usuarios.txt", "a");
+//Abre el archivo en modo lectura, para consultar los registros
+$file = fopen("usuarios.txt", "r");
+//Lee los registros linea por linea
+while(!feof($file)) {
+//Otiene los datos de la linea actual
+$fila= fgetss($file);
+	if(strlen ($fila)!=0){
+	//Separo los datos de la linea en Pipes |
+$datos = explode('|', $fila);
+	
+	//Agrego los datos de la fila en un arreglo bidimensional
+	$usuarios[]= array("nombreCompleto" => $datos[0],"email"=> $datos[1],"direccion"=> $datos[2], "telefono"=>$datos[3], "contras"=>$datos[4]);
+		
+	}
+}
+
+	echo "<div class='form-style-10'>";
+	echo "<h1>Datos del usuario</h1>";
+	echo "<table align='center' width='80%' >";
+	echo "<tbody>";
+foreach($usuarios as $usuario){
+	//Busca si existe el email ingresado desde el formulario
+	$clave = array_search(trim($_POST['email']), $usuario); 
+	
+	if (!empty($clave)){
+		
+		//Valido que la contraseña ingresada desde el formulario sea igual a la del archivo de texto, si es igual muestro los datos del registro previamente encontrado
+		if (trim($usuario['contras'])==trim(sha1($_POST['contras']))){
+			echo "<tr>";
+			echo "<td width='20%' class='header'>Nombre Completo</td>";
+			echo "<td width='80%'>".$usuario['nombreCompleto']."</td>";
+			echo "</tr>";
+			
+			echo "<tr>";
+			echo "<td width='20%' class='header'>Correo Electrónico</td>";
+			echo "<td width='80%'>".$usuario['email']."</td>";
+			echo "</tr>";
+			
+			echo "<tr>";
+			echo "<td width='20%' class='header'>Dirección</td>";
+			echo "<td width='80%'>".$usuario['direccion']."</td>";
+			echo "</tr>";
+			
+			echo "<tr>";
+			echo "<td width='20%' class='header'>Teléfono</td>";
+			echo "<td width='80%'>".$usuario['telefono']."</td>";
+			echo "</tr>";
+			
+			
+			
+			
+		}else{
+						echo "<script type='text/javascript'>alert('Datos incorrectos');window.location.href = 'index.html';</script>";
+		}
+		
+		
+	}
+}
+	echo " </tbody>";
+	echo "</table>";
+	echo "<br/><center><a href='index.html' target='_parent'><input name='Cerrar Sesión' type='submit'  formtarget='_parent' value='Cerrar Sesión' /></a></center>";
+	echo "</div>";
+
+//cierra el archivo abierto.
+fclose($file);
+?>
+</body>
+</html>
---
+++ guardar.php
@@ -0,0 +1,71 @@
+<?php
+
+//Abre el archivo y si no eciste lo crea
+fopen( "usuarios.txt", "a" );
+//Abre el archivo en modo lectura, para consultar los registros
+$file = fopen( "usuarios.txt", "r" );
+//Lee los registros linea por linea
+while ( !feof( $file ) ) {
+	//Otiene los datos de la linea actial
+	$fila = fgetss( $file );
+	//Separo los datos de la linea en Pipes |
+	$datos = explode( '|', $fila );
+	// Verifica si la posición uno (email) existe en el archivo, si ya existe sale del script y muestra una alerta, si no existe continua con el registro
+	if ( $datos[ 1 ] == $_POST[ "email" ] ) {
+		echo "<script type='text/javascript'>alert('El usuario ingresado ya existe');window.location.href = 'registro.html';</script>";
+		exit();
+	}
+
+}
+//cierra el archivo abierto.
+fclose( $file );
+
+
+
+
+
+
+/**
+ *LA siguiente variable permite crear un archivo de texto llamado usuario.txt,
+ *el parametro a: Abre el archivo para sólo escritura. La escritura comenzará al final del archivo sin eliminar el contenido previo existente. Si el fichero no existe se intenta crear. Si por alguna razón
+ *no se puede guardar mostrara una leyenda de error**/
+$archivo = fopen( "usuarios.txt", "a" );
+
+//Declaración de la variable nombreCompleto, que contiene el nombre con apellidos del usuario
+$nombreCompleto = $_POST[ "nombreCompleto" ];
+
+//Declaración de la variable email, que contiene el correo electrónico del usuario
+$email = $_POST[ "email" ];
+//Declaración de la variable direccion, que contiene la dirección del usuario
+$direccion = $_POST[ "direccion" ];
+//Declaración de la variable teléfono, que contiene el teléfono del usuario
+$tel = $_POST[ "tel" ];
+//Declaración de la variable contras, que contiene la primer contraseña escrita por el usuario
+$contras = $_POST[ "contras" ];
+//Declaración de la variable contrasdos, que contiene la segunda contraseña escrita por el usuario, que nos permitira validar que sea igual que la primera
+$contrasdos = $_POST[ "contrasdos" ];
+
+if ( $contras != $contrasdos ) {
+	echo "<script type='text/javascript'>alert('Las contraseñas no coinciden, favor de verificarlas');window.location.href = 'registro.html';</script>";
+
+
+} else {
+
+
+	//Permite crear el archivo si no existe y escribir los datos del usuario escritos en el formulario
+
+	fwrite( $archivo, $nombreCompleto );
+	fwrite( $archivo, "|" );
+	fwrite( $archivo, $email );
+	fwrite( $archivo, "|" );
+	fwrite( $archivo, $direccion );
+	fwrite( $archivo, "|" );
+	fwrite( $archivo, $tel );
+	fwrite( $archivo, "|" );
+	fwrite( $archivo, sha1( trim( $contrasdos ) ); fwrite( $archivo, "\n" );
+
+		fclose( $archivo );
+		//Muestra una alerta donde nos indica que el usuario se ha regisrado de forma correcta
+		echo "<script type='text/javascript'>alert('Usuario creado exitosamente, ahora puede iniciar sesión');window.location.href = 'index.html';</script>";
+	}
+	?>
